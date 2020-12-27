55bc331351f4e518b947f5205a7d54db92acedf7
Kaav-notabot <kaavyaiyer95@gmail.com>
2020-12-27 20:49:25 +0530

    feat: add stop button logic & fixed control toggle

--- background-script.js
+++ background-script.js
@@ -1,10 +1,17 @@
 chrome.runtime.onMessage.addListener( function( request, sender, sendResponse) {
 	switch ( request.control ) {
 		case 'start':
+			console.log()
 			chrome.tabs.executeScript( {
 				file: 'observer.js',
-			} );
+			});
 			sendResponse( { control: true } );
+			break;
+		case 'stop':
+			chrome.tabs.executeScript( {
+				code: 'trade.observer.disconnect();',
+			});
+			sendResponse( { control: false } );
 			break;
 	}
 } );
--- observer.js
+++ observer.js
@@ -1,14 +1,17 @@
-const nodes = document.querySelectorAll( 'h2 + div > span:first-child' );
-const targetNode = nodes[0];
+var trade = ( function() {
+	const nodes = document.querySelectorAll( 'h2 + div > span:first-child' );
+	const targetNode = nodes[0];
 
-var config = { attributes: true, childList: true, subtree: true, characterData: true };
-var callback = function( mutationsList, observer ) {
-	for( var mutation of mutationsList ) {
-		if ( mutation.type === 'characterData' ) {
-			console.log( mutation.target.textContent );
+	var config = { attributes: true, childList: true, subtree: true, characterData: true };
+	var callback = function( mutationsList, observer ) {
+		for( var mutation of mutationsList ) {
+			if ( mutation.type === 'characterData' ) {
+				console.log( mutation.target.textContent );
+			}
 		}
-	}
-};
+	};
 
-var observer = new MutationObserver( callback );
-observer.observe( targetNode, config );
+	var observer = new MutationObserver( callback );
+	observer.observe( targetNode, config );
+	return { observer:observer };
+})();
