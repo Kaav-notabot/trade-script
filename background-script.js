bfe4df1b0816d9b186796c3d015013eea6d0a972
Siddharth Thevaril <siddharththevaril@Siddharths-MacBook-Pro.local>
2020-12-29 13:09:42 +0530

    fix: save observer running state

--- background-script.js
+++ background-script.js
@@ -4,13 +4,23 @@ chrome.runtime.onMessage.addListener( function( request, sender, sendResponse) {
 			chrome.tabs.executeScript( {
 				file: 'observer.js',
 			});
-			sendResponse( { control: true } );
+			chrome.storage.sync.set( { startObserve: true }, function() {
+				sendResponse( { control: true } );
+			} );
+			// chrome.storage.sync.set({key: value}, function() {
+			// 	console.log('Value is set to ' + value);
+			// });
+			// chrome.storage.sync.get(['key'], function(result) {
+			// 	console.log('Value currently is ' + result.key);
+			// });
 			break;
 		case 'stop':
 			chrome.tabs.executeScript( {
 				code: 'trade.observer.disconnect();',
 			});
-			sendResponse( { control: false } );
+			chrome.storage.sync.set( { startObserve: false }, function() {
+				sendResponse( { control: false } );
+			} );
 			break;
 	}
 } );
--- content-script.js
+++ content-script.js
@@ -20,3 +20,19 @@ stopbtn.addEventListener( 'click', function() {
 		startbtn.classList.add( 'row--controls__button--show' );
 	});
 } );
+
+window.onload = function() {
+	chrome.storage.sync.get( ['startObserve'], function( result ) {
+		if ( ! result.startObserve ) {
+			stopbtn.classList = [ 'row--controls__button row--controls__button--stop' ];
+			stopbtn.classList.add( 'row--controls__button--hide' );
+			startbtn.classList = [ 'row--controls__button row--controls__button--start' ];
+			startbtn.classList.add( 'row--controls__button--show' );
+		} else {
+			startbtn.classList = [ 'row--controls__button row--controls__button--start' ];
+			startbtn.classList.add( 'row--controls__button--hide' );
+			stopbtn.classList = [ 'row--controls__button row--controls__button--stop' ];
+			stopbtn.classList.add( 'row--controls__button--show' );
+		}
+	} );
+};
